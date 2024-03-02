159f818e3bbb0797791b6e10f374bb74baa1292b
MessaKami <messaoud.houri@outlook.fr>
2024-03-02 19:16:22 +0100

    Ajout du role

---
+++ src/Form/UserType.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('email', EmailType::class, [
+            'attr' => [
+                'class' => 'form-control'
+            ],
+            'label' => 'E-mail'
+        ])
+            ->add('roles', ChoiceType::class, [
+                'choices'  => [
+                    'ROLE_ADMIN' => 'ROLE_ADMIN',
+                    'ROLE_SENIOR' => 'ROLE_SENIOR',
+                    'ROLE_APPRENTI' => 'ROLE_APPRENTI',
+                    'ROLE_CLIENT' => 'ROLE_CLIENT',
+                    'ROLE_USER' => 'ROLE_USER',
+                ],
+                'expanded' => false,
+                'multiple' => false,
+                'label' => 'Roles',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            // ->add('password')
+            // ->add('google_id')
+            ->add('avatar')
+            ->add('lastname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Nom'
+            ])
+            ->add('firstname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Prénom'
+            ])
+            ->add('tel', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Téléphone'
+            ])
+            // ->add('created_at')
+            // ->add('operations_finalisee')
+            ->add('address', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Adresse'
+            ])
+            ->get('roles')
+
+            ->addModelTransformer(new CallbackTransformer(
+                fn ($rolesAsArray) => count($rolesAsArray) ? $rolesAsArray[0]: null,
+                fn ($rolesAsString) => [$rolesAsString]
+        ));
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
