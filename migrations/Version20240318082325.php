9f155c3b694e0c188aaa7d732b9b232c379009f2
Messa <78752191+MessaKami@users.noreply.github.com>
2024-03-18 13:31:26 +0100

    Upload photo pour les services

--- migrations/Version20240318082325.php
+++ migrations/Version20240318082325.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240312132233 extends AbstractMigration
+final class Version20240318082325 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20240312132233 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE type_operation ADD image LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user DROP mail_token');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `user` DROP is_verified');
+        $this->addSql('ALTER TABLE type_operation DROP image');
+        $this->addSql('ALTER TABLE `user` ADD mail_token VARCHAR(255) DEFAULT NULL');
     }
 }
