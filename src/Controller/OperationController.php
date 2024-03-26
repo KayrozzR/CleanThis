5e06bba8f0542c7c51495df27caf3521b4bbaf13
MessaKami <messaoud.houri@outlook.fr>
2024-03-26 21:00:38 +0100

    Ajout des attributions des opérations pour l'admin

--- src/Controller/OperationController.php
+++ src/Controller/OperationController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\Operation;
 use App\Form\OperationType;
 use App\Form\ReclamationType;
+use App\Repository\UserRepository;
 use App\Repository\OperationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/operation')]
@@ -79,6 +82,77 @@ class OperationController extends AbstractController
 
     //     return $this->redirectToRoute('app_operation_index', [], Response::HTTP_SEE_OTHER);
     // }
+
+
+    #[Route('/assign/{id}', name: 'app_operation_assign', methods: ['GET', 'POST'])]
+    public function assignOperation(Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository, Operation $operation): Response
+    {
+        $roles = ['ROLE_SENIOR', 'ROLE_APPRENTI', 'ROLE_ADMIN'];
+        $employees = $userRepository->findByRoles($roles); 
+
+        $form = $this->createFormBuilder()
+            ->add('user', ChoiceType::class, [
+                'choices' => $employees,
+                'choice_label' => function(?User $user) {
+                    return $user ? $user->getFirstname() . ' ' . $user->getLastname() . ' (' . implode(', ', array_diff($user->getRoles(), ['ROLE_USER'])) . ')' : '';
+                },
+            ])
+            ->getForm();
+    
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $selectedUser = $form->getData()['user'];
+
+            if (in_array('ROLE_ADMIN', $selectedUser->getRoles(), true) && $selectedUser->getOperationEnCours() >= 5) {
+                $this->addFlash('error', 'L\'administrateur a déjà atteint le nombre maximum d\'opérations en cours.');
+                return $this->redirectToRoute('app_operation_index');
+            } elseif (in_array('ROLE_SENIOR', $selectedUser->getRoles(), true) && $selectedUser->getOperationEnCours() >= 3) {
+                $this->addFlash('error', 'Le sénior a déjà atteint le nombre maximum d\'opérations en cours.');
+                return $this->redirectToRoute('app_operation_index');
+            } elseif (in_array('ROLE_APPRENTI', $selectedUser->getRoles(), true) && $selectedUser->getOperationEnCours() >= 1) {
+                $this->addFlash('error', 'L\'apprenti a déjà atteint le nombre maximum d\'opérations en cours.');
+                return $this->redirectToRoute('app_operation_index');
+            }
+    
+            // Décrémenter pour l'utilisateur actuel, s'il existe
+            if ($operation->getUser() !== null && $operation->getUser()->getId() !== $selectedUser->getId()) {
+                $currentUser = $operation->getUser();
+                $currentUser->setOperationEnCours(max($currentUser->getOperationEnCours() - 1, 0));
+                $entityManager->persist($currentUser);
+            }
+    
+            // Incrémenter pour le nouvel utilisateur
+            $selectedUser->setOperationEnCours($selectedUser->getOperationEnCours() + 1);
+            $entityManager->persist($selectedUser);
+    
+            $operation->setUser($selectedUser);
+            $entityManager->flush();
+    
+            return $this->redirectToRoute('app_operation_index');
+        }
+    
+        return $this->render('operation/assign.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/unassign/{id}', name: 'app_operation_unassign', methods: ['POST'])]
+public function unassignOperation(Request $request, EntityManagerInterface $entityManager, Operation $operation): Response
+{
+    if ($this->isCsrfTokenValid('unassign'.$operation->getId(), $request->request->get('_token'))) {
+        if ($operation->getUser() !== null) {
+            $currentUser = $operation->getUser();
+            $currentUser->setOperationEnCours(max($currentUser->getOperationEnCours() - 1, 0));
+            $entityManager->persist($currentUser);
+        }
+
+        $operation->setUser(null);
+        $entityManager->flush();
+    }
+
+    return $this->redirectToRoute('app_operation_index');
+}
 
 
 }
